824b2c7cea74e84a97dca0181cdd5d214418ebba
Paola Lopez <paola.lopez1@yahoo.com>
2021-03-29 17:50:25 -0700

    editing my flask app as the example I followed wasn't 100% applicable to article text, still researching how to scrape article text and run through model

--- paola/app.py
+++ paola/app.py
@@ -1,8 +1,19 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template, redirect
+from flask_cors import CORS
+import os
+from sklearn.externals import joblib
 import pickle
+import newspaper
+from newspaper import Article
+import urllib
+import nltk
+# Download punkt sentence tokenizer
+nltk.download('punkt')
 
+#Load flask and model
 app = Flask(__name__)
+CORS
 model = pickle.load(open('fake_news_model.pickle','rb'))
 
 ####################################
@@ -14,12 +25,16 @@ def homepage():
     return render_template("index.html")
 
 # Route for model
-@app.route("/api", methods=['POST'])
+@app.route("/detect", methods=['GET', 'POST'])
 def predict():
-    data = request.get_json(force=True)
-    prediction = model.predict([[np.array(data['exp'])]])
-    output = prediction[0]
-    return jsonify(output)
+
+
+    news = article.summary
+
+    # Passing the news article and returning if it is 'Real' or 'Fake'
+    prediction = model.predict([news])
+    return render_template('index.html', article_text = 'This article is "{}"'.format(pred[0]))
+
 
 
 if __name__ == '__main__':
