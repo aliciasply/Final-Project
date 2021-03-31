75038293d2505671ce092ddb5f2b84851ab85dfd
Miguel Godfrey Eamiguel <70498636+52Godfrey@users.noreply.github.com>
2021-03-30 19:40:40 -0700

    app.py

--- Final Code/app.py
+++ Final Code/app.py
@@ -1,8 +1,17 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template, redirect, send_from_directory
 import pickle
+from flask_cors import CORS
+import os
+import newspaper
+from newspaper import Article
+import urllib
+import nltk
+# Download punkt sentence tokenizer
+nltk.download('punkt')
 
 app = Flask(__name__, template_folder='templates', static_url_path='')
+CORS
 model = pickle.load(open('fake_news_model.pickle','rb'))
 
 ####################################
@@ -16,12 +25,20 @@ def home():
 
 
 # Route for model
-@app.route("/api", methods=['POST'])
+@app.route("/detect", methods=['GET', 'POST'])
 def predict():
-    data = request.get_json(force=True)
-    prediction = model.predict([[np.array(data['exp'])]])
-    output = prediction[0]
-    return jsonify(output)
+    url = request.get_data(as_text = True)
+    url = urllib.parse.unquote(url)
+    article = Article(str(url))
+    article.download()
+    article.parse()
+    article.nlp()
+
+    news = article.summary
+
+    # Passing the news article and returning if it is 'Real' or 'Fake'
+    prediction = model.predict([news])
+    return render_template('index4.html', article_text = 'This article is "{}"'.format(prediction[0]))
 
 
 if __name__ == '__main__':
