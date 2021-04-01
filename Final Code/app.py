7c1c869eb9416876cc5f7335909aef3a8d6db4ad
Miguel Godfrey Eamiguel <70498636+52Godfrey@users.noreply.github.com>
2021-03-31 19:12:36 -0700

    Updated Flask

--- Final Code/app.py
+++ Final Code/app.py
@@ -13,7 +13,6 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 tfidf_vectorizer = TfidfVectorizer(stop_words='english', max_df=0.7)
 import pandas as pd
 
-
 app = Flask(__name__, template_folder='templates', static_url_path='')
 CORS(app)
 model = pickle.load(open('fake_news_model.pickle','rb'))
@@ -22,19 +21,57 @@ xy_train = pd.read_csv("./xy_train.csv")
 x_train_vec = tfidf_vectorizer.fit_transform(xy_train['text'])
 model.fit(x_train_vec, xy_train['label'])
 
-
 ####################################
 # Flask Routes
 ####################################
 # HOMEPAGE
 @app.route("/")
-@app.route("/index4.html")
+@app.route("/index.html")
 def home():
-    return render_template("/index4.html")
+    return render_template("/index.html")
 
+@app.route("/visualizations.html")
+def visualizations():
+    return render_template("/visualizations.html")
+
+@app.route("/base.html")
+def data():
+    return render_template("/base.html")
+
+@app.route("/all_news_summary.html")
+def all_news_summary():
+    return render_template("/all_news_summary.html")
+
+@app.route("/fake_real_count.html")
+def fake_real_count():
+    return render_template("/fake_real_count.html")
+
+@app.route("/top5real_likebar.html")
+def top5real_likebar():
+    return render_template("/top5real_likebar.html")
+
+@app.route("/top5Fake_likebar.html")
+def top5Fake_likebar():
+    return render_template("/top5Fake_likebar.html")
+
+@app.route("/top5real_commentbar.html")
+def top5real_commentbar():
+    return render_template("/top5real_commentbar.html")
+
+@app.route("/top5Fake_commentbar.html")
+def top5Fake_commentbar():
+    return render_template("/top5Fake_likebar.html")
+
+@app.route("/top5real_sharebar.html")
+def top5real_sharebar():
+    return render_template("/top5real_sharebar.html")
+
+@app.route("/top5Fake_sharebar.html")
+def top5Fake_sharebar():
+    return render_template("/top5Fake_sharebar.html")
 
 # Route for model
-@app.route("/results", methods=['GET', 'POST'])
+@app.route("/result", methods=['GET', 'POST'])
 def predict():
     
     # url = request.get_data(as_text = True)[5:]
@@ -53,7 +90,9 @@ def predict():
     print("=========================")
     print(prediction)
     print("=========================")
-    return render_template('index4.html', article_text = 'This article is "{}"'.format(prediction[0]))
+    return render_template('index.html', article_text = 'This article is "{}"'.format(prediction[0]))
+
+
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
